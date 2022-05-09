133b0b4de74753767537dedfb1a108b09b70b99a
Svetoslav2112 <57129022+Svetoslav2112@users.noreply.github.com>
2022-05-09 12:55:31 +0300

    Basic step by step guide
    Initial publish

---
+++ javascript/dropdown.js
@@ -0,0 +1,23 @@
+var collapsible = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < collapsible.length; i++) {
+
+  collapsible[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var steps = this.nextElementSibling;
+
+    if (steps.style.maxHeight) {
+      steps.style.maxHeight = null;
+      document.querySelectorAll('nav').forEach(function (e) {
+        e.style.transition = "1.5s"
+        e.style.top = '30%';
+      })
+    } else {
+      steps.style.maxHeight = steps.scrollHeight + 50 + "px";
+      document.querySelectorAll('nav').forEach(function (e) {
+        e.style.transitionDuration = "0.5s"
+        e.style.top = '18%';
+      })
+    }
+  })
+}
